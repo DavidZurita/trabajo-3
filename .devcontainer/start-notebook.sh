512905ee89d2a8b5cde0a59f6c14050bf8320f80
Vicente David Zurita Rodriguez <vizuritaro@uide.edu.ec>
2025-03-11 21:28:35 -0500

    wip:proceso

--- .devcontainer/start-notebook.sh
+++ .devcontainer/start-notebook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jupyter notebook --NotebookApp.password='' --NotebookApp.ip='0.0.0.0' --NotebookApp.port=8888 --no-browser --allow-root
+jupyter notebook --NotebookApp.password='12345678' --NotebookApp.ip='0.0.0.0' --NotebookApp.port=8888 --no-browser --allow-root
